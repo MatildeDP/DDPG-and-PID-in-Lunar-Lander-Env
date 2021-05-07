dd21e29e97c52d43bc6bf0224347aedd7d29c0df
s194274 <82712451+s194274@users.noreply.github.com>
2021-05-07 11:46:54 +0200

    Delete irlc directory

--- irlc/utils/df_video_monitor.py
+++
@@ -1,6 +0,0 @@
-"""
-This file may not be shared/redistributed without permission. Please read copyright notice in the git repo. If this file contains other copyright notices disregard this text.
-"""
-s = "234234"
-# class VideoMonitorC:
-#     pass
